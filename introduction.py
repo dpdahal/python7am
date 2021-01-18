37eaacdafc61178a65a9157f9b14929705bd0d2a
dpdahal <dp48dahal@gmail.com>
2021-01-18 07:54:23 +0545

    'test'

--- introduction.py
+++ introduction.py
@@ -9,11 +9,16 @@
 # a = 10
 # b = 'ram'
 
-name = "Sophia"
-age = 10
-print("Name:", name, "age", age)
-print(f"Name:{name} age:{age}")
+# name = "Sophia"
+# age = 10
+# print("Name:", name, "age", age)
+# print(f"Name:{name} age:{age}")
 # print("My Name is "+name+ "age"+age)
-print("My Name: {} Age: {}".format(name, age))  # Old
+# print("My Name: {} Age: {}".format(name, age))  # Old
 # print(type (name))
 # print(dir(age))
+
+
+x = 10
+
+print(x)
