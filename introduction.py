b84b47b7b70a24762cd647cb63f7481bfe19b7c6
dpdahal <dp48dahal@gmail.com>
2021-01-18 07:40:13 +0545

    test

---
+++ introduction.py
@@ -0,0 +1,19 @@
+# print("Hello Python")
+
+# print('mother\'s')
+#
+# print("mother's")
+
+# print("hello", 'Python')
+#
+# a = 10
+# b = 'ram'
+
+name = "Sophia"
+age = 10
+print("Name:", name, "age", age)
+print(f"Name:{name} age:{age}")
+# print("My Name is "+name+ "age"+age)
+print("My Name: {} Age: {}".format(name, age))  # Old
+# print(type (name))
+# print(dir(age))
